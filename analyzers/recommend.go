336190f5ef075dd3c10bf0e9b5e2e0aa5a6ef611
Takuya Ueda <uedatakuya@gmail.com>
2019-03-08 21:01:37 +0900

    Move from tenntenn/gosa

---
+++ analyzers/recommend.go
@@ -0,0 +1,18 @@
+package analyzers
+
+import (
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/gostaticanalysis/nilerr"
+	"github.com/gostaticanalysis/readonly"
+	"github.com/gostaticanalysis/wraperrfmt"
+)
+
+// Recommend returns recommended analyzers including govet's one.
+func Recommend() []*analysis.Analyzer {
+	return append(Govet(),
+		nilerr.Analyzer,
+		wraperrfmt.Analyzer,
+		readonly.Analyzer,
+	)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tenntenn/gosa/vetgen
+
+go 1.12
+
+require (
+	github.com/gostaticanalysis/nilerr v0.0.0-20190308085927-d5e696fc40f8
+	github.com/gostaticanalysis/readonly v0.0.0-20190308111141-902245d4df63
+	github.com/gostaticanalysis/wraperrfmt v0.0.0-20190308090456-74aac36802ea
+	golang.org/x/tools v0.0.0-20190307163923-6a08e3108db3
+)
