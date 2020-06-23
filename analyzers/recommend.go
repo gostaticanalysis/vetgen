711884e5a2a3b14b3bbef38306aa07570b181d5f
Takuya Ueda <uedatakuya@gmail.com>
2020-06-23 13:51:45 +0900

    Update recommended analyzers and govet's analyzers

--- analyzers/recommend.go
+++ analyzers/recommend.go
@@ -3,22 +3,30 @@ package analyzers
 import (
 	"golang.org/x/tools/go/analysis"
 
-	"github.com/gostaticanalysis/fourcetypeassert"
+	"github.com/gostaticanalysis/ctxfield"
+	"github.com/gostaticanalysis/dupimport"
+	"github.com/gostaticanalysis/forcetypeassert"
+	"github.com/gostaticanalysis/importgroup"
 	"github.com/gostaticanalysis/nilerr"
 	"github.com/gostaticanalysis/nofmt"
 	"github.com/gostaticanalysis/notest"
 	"github.com/gostaticanalysis/readonly"
-	"github.com/gostaticanalysis/wraperrfmt"
+	"github.com/gostaticanalysis/unitconst"
+	"github.com/gostaticanalysis/unused"
 )
 
 // Recommend returns recommended analyzers including govet's one.
 func Recommend() []*analysis.Analyzer {
 	return append(Govet(),
+		ctxfield.Analyzer,
+		dupimport.Analyzer,
+		forcetypeassert.Analyzer,
+		importgroup.Analyzer,
 		nilerr.Analyzer,
-		wraperrfmt.Analyzer,
-		readonly.Analyzer,
 		nofmt.Analyzer,
 		notest.Analyzer,
-		fourcetypeassert.Analyzer,
+		readonly.Analyzer,
+		unitconst.Analyzer,
+		unused.Analyzer,
 	)
 }
